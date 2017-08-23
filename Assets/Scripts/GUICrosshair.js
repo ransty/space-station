a4fe4f9133f615804a380e6aa7a81f7e9b1d9dc2
Porcaro <porkd002@f120-02.local>
2017-08-23 13:51:53 +0930

    GUI script

---
+++ Assets/Scripts/GUICrosshair.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+var crosshairTexture : Texture2D;
+var position : Rect;
+static var OriginalOn = true;
+
+function Start () {
+	position = Rect((Screen.width - crosshairTexture.width) / 2, (Screen.height - crosshairTexture.height) / 2, crosshairTexture.width, crosshairTexture.height));
+}
+
+function Update () {
+	if (OriginalOn) { 
+		GUI.DrawTexture(position, crosshairTexture);
+	}
+}
