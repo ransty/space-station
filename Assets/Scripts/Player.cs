3c053cec1dafa3aa92a6bed46ba9461201bfc69e
Keano Porcaro <keano.porcaro@gmail.com>
2017-08-24 12:03:37 +0930

    steamvr + walking/running

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Player : MonoBehaviour {
 
@@ -10,11 +11,20 @@ public class Player : MonoBehaviour {
 
 	private bool hasJumped;
 
+	private SteamVR_TrackedObject trackedObj;
+
+	private SteamVR_Controller.Device Controller {
+		get { return SteamVR_Controller.Input ((int)trackedObj.index); }
+	}
+
 	public GameObject eyes;
 
 	float moveFB, moveLR, verticalVelocity, rotX, rotY;
 
 
+	void Awake() {
+		trackedObj = GetComponent<SteamVR_TrackedObject> ();
+	}
 
 	// Use this for initialization
 	void Start () {
@@ -39,9 +49,50 @@ public class Player : MonoBehaviour {
 		if (Input.GetButtonDown ("Jump")) {
 			hasJumped = true;
 		}
+
+		//Debug.Log(Controller.GetAxis());
+		if (Controller.GetAxis().y > 0.1 && Controller.GetAxis().y < 0.5)
+		{
+			player.transform.position = player.transform.position + Camera.main.transform.forward * 0.01f;            
+		}
+		else if(Controller.GetAxis().y > -0.5 && Controller.GetAxis().y < -0.1)
+		{
+			player.transform.position = player.transform.position - Camera.main.transform.forward * 0.01f;
+		}
+		else if (Controller.GetAxis().y >= 0.5)
+		{
+			player.transform.position = player.transform.position + Camera.main.transform.forward * 0.04f;
+		}
+		else if (Controller.GetAxis().y <= -0.5)
+		{
+			player.transform.position = player.transform.position - Camera.main.transform.forward * 0.04f;
+		}
+
+		if (Controller.GetAxis().x > 0.1 && Controller.GetAxis().x < 0.5)
+		{
+			player.transform.position = player.transform.position - Vector3.Cross(Camera.main.transform.forward, Camera.main.transform.up) * 0.01f;
+		}
+		else if (Controller.GetAxis().x > -0.5 && Controller.GetAxis().x < -0.1)
+		{
+			player.transform.position = player.transform.position + Vector3.Cross(Camera.main.transform.forward, Camera.main.transform.up) * 0.01f;
+		}
+		else if (Controller.GetAxis().x >= 0.5)
+		{
+			player.transform.position = player.transform.position - Vector3.Cross(Camera.main.transform.forward, Camera.main.transform.up) * 0.04f;
+		}
+		else if (Controller.GetAxis().x <= -0.5)
+		{
+			player.transform.position = player.transform.position + Vector3.Cross(Camera.main.transform.forward, Camera.main.transform.up) * 0.04f;
+		}
 	}
 
 	void Movement() {
+		// playerSpeed = controller pad direction
+		// -1 and 1 LR, FB
+		// if Axis2D.PrimaryThumbstick.ID == 1 // horizontal
+		// if Axis2D.PrimaryThumbstick.ID == 2 // Vertical
+		// 4 horizontal
+		// 5 vertical
 		moveFB = Input.GetAxis("Vertical") * playerSpeed;
 		moveLR = Input.GetAxis("Horizontal") * playerSpeed;
 
